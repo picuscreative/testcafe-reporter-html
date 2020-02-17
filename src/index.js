2e1b2da589a02a90416590426e0f437d5ad34790
Selvarajan <selvarajaneeesrit@gmail.com>
2020-02-17 16:21:35 +0530

    Comments are being addressed

--- src/index.js
+++ src/index.js
@@ -186,7 +186,7 @@ module.exports = () => ({
     html += `
       <h1 class="text-primary">TestCafe Test Summary</h1>
       <div class="client-logo" style="padding:15px"></div>
-      <div class="bg-primary" style="padding:15px;box-shadow: 10px 10px 10px 10px #00000033;border-radius: 10px;">
+      <div class="bg-primary" style="padding:15px;box-shadow: 6px 6px 6px 6px #00000033;border-radius: 10px;">
         <h3>Summary</h3><br>
         <p class="lead">Start Time: ${this.startTime}</p>
         <p class="lead">Browsers: ${this.uaList}</p>
@@ -197,7 +197,7 @@ module.exports = () => ({
 
     // Summary table
     html += `
-      <table class="table sortable" style="padding:15px;box-shadow: 10px 10px 10px 10px #00000033;border-radius: 10px;">
+      <table class="table sortable" style="padding:15px;box-shadow: 6px 6px 6px 6px #00000033;border-radius: 10px;">
         <thead>
           <tr>
             <th>#</th>
@@ -222,8 +222,7 @@ module.exports = () => ({
           </tr>
         </thead>
       </table>
-      <br>
-      `;
+      <br>`;
 
     // Error details
     html += `
